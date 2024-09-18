a28a1901bda3b3f3d25cf9c5b68190ec6dededfa
Mariana Trinko <marjanavv@gmail.com>
2024-09-18 15:27:41 +0200

    message

--- src/app/guards/special-guard.guard.ts
+++ src/app/guards/special-guard.guard.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 
 import { LoginServiceService } from '../services/login-service.service';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChildFn, CanActivateFn, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChildFn, CanActivateFn, GuardResult, MaybeAsync, Route, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class specialGuardGuard implements CanActivate {
@@ -12,7 +13,9 @@ export class specialGuardGuard implements CanActivate {
 
   }
 
-
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return this.loginService.isLogin;
+  }
 
 }
 
